d023e1c975830b2c352427ad92ad4b9066ab87c0
Jmef19 <juanmaef2003@gmail.com>
2024-12-03 09:40:28 +0100

    changed icon

--- src/app/layout.js
+++ src/app/layout.js
@@ -23,6 +23,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        <link rel="icon" href="/icon.png" />
+      </Head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
